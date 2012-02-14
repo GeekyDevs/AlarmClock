bf6e6299e174db5390b71fd42904f2b707db247f
Simon Ng <simonng47@rogers.com>
2012-02-14 12:17:53 -0500

    Updated the alarm settings UI and handled the changes made in repeat and time selection to reflect in the alarm setting view.

--- GeekyClock/src/com/geekydevs/alarmclock/AlarmEdit.java
+++ GeekyClock/src/com/geekydevs/alarmclock/AlarmEdit.java
@@ -4,29 +4,37 @@ import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.DatePicker;
-
 
 public class AlarmEdit extends Activity {
 
 	private static final int DIALOG_PICK_TIME = 1;
-	private static final int DIALOG_PICK_DATE = 2;
 	
 	private static final int ACTION_INPUT_LABEL = 1;
 	private static final int ACTION_CHOOSE_REPEAT = 3;
 	
 	private LinearLayout lLRepeat;
 	private LinearLayout lLTime;
-	private LinearLayout lLDate;
 	private LinearLayout lLLabel;
 	
+	private TextView timeView;
+	private TextView repeatView;
+	
+	
+	
+	/*
+	 * Automatically called when AlarmEdit was created and launches the UI to
+	 * the alarm edit screen. 
+	 * Sets up the view and listener.
+	 * 
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,24 +44,60 @@ public class AlarmEdit extends Activity {
 		assignListeners();
 	}
 	
+	/*
+	 * Assigns each item in the UI that requires interaction to the proper 
+	 * variable.
+	 */
 	private void findViews() {
 		
 		lLLabel = (LinearLayout)findViewById(R.id.ea_ll_label);
 		
 		lLRepeat = (LinearLayout)findViewById(R.id.ea_ll_alarm_repeat);
 		lLTime = (LinearLayout)findViewById(R.id.ea_ll_alarm_time);
-		lLDate = (LinearLayout)findViewById(R.id.ea_ll_alarm_date);
+		
+		timeView = (TextView)findViewById(R.id.time_selection);
+		repeatView = (TextView)findViewById(R.id.repeat_selection);
 	}
 	
+	/*
+	 * Set up each variable that was assigned a view with a listener.
+	 */
 	private void assignListeners() {
 		
 		lLLabel.setOnClickListener(labelOnClick);
 		
 		lLRepeat.setOnClickListener(repeatOnClick);
 		lLTime.setOnClickListener(timeOnClick);
-		lLDate.setOnClickListener(dateOnClick);
 	}
 	
+	/*
+	 * Updates the time to reflect the change made by the user.
+	 */
+	private void updateTimeView (String time) {
+		timeView.setText(time);
+	}
+	
+	private void updateRepeatView (String choices) {
+		repeatView.setText(choices);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case ACTION_CHOOSE_REPEAT:
+				updateRepeatView(data.getStringExtra("days"));
+				break;
+			}
+		}
+		
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	/*
+	 * 
+	 */
 	private View.OnClickListener labelOnClick = new View.OnClickListener() {
 		
 		@Override
@@ -69,17 +113,11 @@ public class AlarmEdit extends Activity {
 		Calendar c = Calendar.getInstance(); 
 		int minute = c.get(Calendar.MINUTE);
 		int hour = c.get(Calendar.HOUR);
-		
-		int year = c.get(Calendar.YEAR);
-		int month = c.get(Calendar.MONTH);
-		int day = c.get(Calendar.DAY_OF_MONTH);
 
 		switch (id)
 		{
 		case DIALOG_PICK_TIME:
 			return new TimePickerDialog(this, mOnTimeSetListener, hour, minute, false);
-		case DIALOG_PICK_DATE:
-			return new DatePickerDialog(this, mOnDateSetListener, year, month, day);
 		}
 		
 		return super.onCreateDialog(id);
@@ -89,7 +127,30 @@ public class AlarmEdit extends Activity {
 
 		@Override
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-			//do nothing
+		
+			String timeFormat = "";
+			
+			if (hourOfDay > 12) {
+				timeFormat += (hourOfDay-12) + ":" ;
+			} else if (hourOfDay == 0) {
+				timeFormat += (hourOfDay + 12) + ":";
+			} else {
+				timeFormat += hourOfDay + ":";
+			}
+			
+			if (minute < 10) {
+				timeFormat += "0" + minute;
+			} else {
+				timeFormat += minute;
+			}
+			
+			if (hourOfDay >= 12) {
+				timeFormat += " pm";
+			} else {
+				timeFormat += " am";
+			}
+		
+			updateTimeView(timeFormat);
 		}
 	};
 	
@@ -101,27 +162,12 @@ public class AlarmEdit extends Activity {
 		}
 	};
 	
-	private DatePickerDialog.OnDateSetListener mOnDateSetListener =
-        new DatePickerDialog.OnDateSetListener() {
-
-			@Override
-			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-				//do nothing
-			}
-	};
-	
-	private View.OnClickListener dateOnClick = new View.OnClickListener() {
-		
-		@Override
-		public void onClick (View view) {
-			showDialog(DIALOG_PICK_DATE);
-		}
-	};
 	
 	private View.OnClickListener repeatOnClick = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			Intent i = new Intent(getBaseContext(), RepeatSelection.class);
+			i.putExtra("days", repeatView.getText());
 			startActivityForResult(i, ACTION_CHOOSE_REPEAT);
 		}
 	};
--- GeekyClock/src/com/geekydevs/alarmclock/RepeatSelection.java
+++ GeekyClock/src/com/geekydevs/alarmclock/RepeatSelection.java
@@ -9,20 +9,88 @@ import android.widget.CheckBox;
 
 public class RepeatSelection extends Activity {
 	
-	private CheckBox sun_check;
-	private CheckBox mon_check;
-	private CheckBox tue_check;
-	private CheckBox wed_check;
-	private CheckBox thu_check;
-	private CheckBox fri_check;
-	private CheckBox sat_check;
+	private CheckBox chkSun;
+	private CheckBox chkMon;
+	private CheckBox chkTue;
+	private CheckBox chkWed;
+	private CheckBox chkThu;
+	private CheckBox chkFri;
+	private CheckBox chkSat;
 	
+	/*
+	 * Clicking the 
+	 */
+	private Button.OnClickListener okOnClick = new Button.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			
+			String daysChecked = "";
+			
+			if (chkMon.isChecked()) {daysChecked += "Monday, ";}
+			if (chkTue.isChecked()) {daysChecked += "Tuesday, ";}
+			if (chkWed.isChecked()) {daysChecked += "Wednesday, ";}
+			if (chkThu.isChecked()) {daysChecked += "Thursday, ";}
+			if (chkFri.isChecked()) {daysChecked += "Friday, ";}
+			if (chkSat.isChecked()) {daysChecked += "Saturday, ";}
+			if (chkSun.isChecked()) {daysChecked += "Sunday, ";}
+			
+			if (daysChecked == "") {
+				daysChecked = "Never";
+			} else {
+				daysChecked = daysChecked.substring(0, daysChecked.length()-2);
+			}
 
+			Intent i = new Intent();
+			i.putExtra("days", daysChecked);
+
+			setResult(Activity.RESULT_OK, i);
+			finish();
+		}
+	};
+	
+	private Button.OnClickListener cancelOnClick = new Button.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			setResult(Activity.RESULT_CANCELED);
+			finish();
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.repeat_selection);
 		setTitle("Alarm Repeats...");
+		
+		chkMon = (CheckBox)findViewById(R.id.rpt_mon);
+		chkTue = (CheckBox)findViewById(R.id.rpt_tues);
+		chkWed = (CheckBox)findViewById(R.id.rpt_wed);
+		chkThu = (CheckBox)findViewById(R.id.rpt_thur);
+		chkFri = (CheckBox)findViewById(R.id.rpt_fri);
+		chkSat = (CheckBox)findViewById(R.id.rpt_sat);
+		chkSun = (CheckBox)findViewById(R.id.rpt_sun);
+
+		findViewById(R.id.sr_btn_ok).setOnClickListener(okOnClick);
+		findViewById(R.id.sr_btn_cancel).setOnClickListener(cancelOnClick);
+		
+		String oldSetting = "";
+		
+		Bundle b = getIntent().getExtras();
+		oldSetting = b.getString("days");
+		
+		if (oldSetting.indexOf("Monday") >= 0) {chkMon.setChecked(true);}
+		if (oldSetting.indexOf("Tuesday") >= 0) {chkTue.setChecked(true);}
+		if (oldSetting.indexOf("Wednesday") >= 0) {chkWed.setChecked(true);}
+		if (oldSetting.indexOf("Thursday") >= 0) {chkThu.setChecked(true);}
+		if (oldSetting.indexOf("Friday") >= 0) {chkFri.setChecked(true);}
+		if (oldSetting.indexOf("Saturday") >= 0) {chkSat.setChecked(true);}
+		if (oldSetting.indexOf("Sunday") >= 0) {chkSun.setChecked(true);}
+		
 	}
+	
+	
 }
 
